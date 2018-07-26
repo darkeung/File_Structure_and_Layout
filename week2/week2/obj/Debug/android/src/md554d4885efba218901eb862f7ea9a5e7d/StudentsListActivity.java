d7744e29b287a40ad47bc6238c5ddcfc040ab5fa
darkeung <hawygfau@gmail.com>
2018-07-26 10:10:05 +1000

    work

---
+++ week2/week2/obj/Debug/android/src/md554d4885efba218901eb862f7ea9a5e7d/StudentsListActivity.java
@@ -0,0 +1,47 @@
+package md554d4885efba218901eb862f7ea9a5e7d;
+
+
+public class StudentsListActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("week2.StudentsListActivity, week2", StudentsListActivity.class, __md_methods);
+	}
+
+
+	public StudentsListActivity ()
+	{
+		super ();
+		if (getClass () == StudentsListActivity.class)
+			mono.android.TypeManager.Activate ("week2.StudentsListActivity, week2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ week2/week2/obj/Debug/android/src/md5f150a4e399b591c52576ff343f675e7f/Details.java
@@ -0,0 +1,56 @@
+package md5f150a4e399b591c52576ff343f675e7f;
+
+
+public class Details
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("week2.TesterApp.Details, week2", Details.class, __md_methods);
+	}
+
+
+	public Details ()
+	{
+		super ();
+		if (getClass () == Details.class)
+			mono.android.TypeManager.Activate ("week2.TesterApp.Details, week2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onActivityResult (int p0, int p1, android.content.Intent p2)
+	{
+		n_onActivityResult (p0, p1, p2);
+	}
+
+	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
